efb281c372102d78de7361f559f50d205bfa6404
Harish-Rusum <harish.rusum@gmail.com>
2024-06-04 12:46:41 +0530

    added a flash when text is yanked

--- lua/user/plugins/completions.lua
+++ lua/user/plugins/completions.lua
@@ -14,7 +14,7 @@ cmp.setup({
 	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<Tab>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<Escape>"] = cmp.mapping.abort(),
 		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.

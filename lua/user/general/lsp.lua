2b0ea1677d85786e60f44886c71460ab35fb991d
Harish-Rusum <harish.rusum@gmail.com>
2024-06-07 09:22:12 +0530

    changed error symbols to something better

--- lua/user/general/lsp.lua
+++ lua/user/general/lsp.lua
@@ -1,5 +1,5 @@
 -- Set diagnostic signs
-local signs = { Error = "?", Warn = " ", Info = " " }
+local signs = { Error = "❗", Warn = " ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })

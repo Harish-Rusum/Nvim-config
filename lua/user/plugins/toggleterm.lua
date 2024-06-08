2faaefe52ef01f7ba7a5630b9de8df1412070ca9
Harish-Rusum <harish.rusum@gmail.com>
2024-06-08 18:10:27 +0530

    made toggleterm rounded

--- lua/user/plugins/toggleterm.lua
+++ lua/user/plugins/toggleterm.lua
@@ -1,6 +1,10 @@
 -- PERF: setting toggleterm (terminal) default shell to powershell
 
-require('toggleterm').setup{
-	persist_size = true, -- Persist the terminal size between uses
-	shell = "pwsh", -- Use the default shell
+require('toggleterm').setup {
+    size = 20,
+    float_opts = {
+        border = 'rounded',
+    },
+    persist_size = true, -- Persist the terminal size between uses
+    shell = "pwsh", -- Use the default shell
 }

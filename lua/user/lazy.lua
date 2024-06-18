b3352d65f724e74529d4f63d589d2c56d1772277
Harish-Rusum <harish.rusum@gmail.com>
2024-06-18 21:49:26 +0530

    added a comma

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -141,7 +141,7 @@ local plugins = {
 	    "NStefan002/screenkey.nvim",
 	    lazy = false,
 	    version = "*", -- or branch = "dev", to use the latest commit
-	}
+	},
 }
 -- PERF: setting a rounded border
 

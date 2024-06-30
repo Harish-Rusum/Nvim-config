4de20f6cb62645aaac741b91cf2a78bb3223b711
harish.rusum@gmail.com <Harish-Rusum>
2024-06-30 13:19:39 +0530

    made changes to the yank system

--- lua/user/general/settings.lua
+++ lua/user/general/settings.lua
@@ -4,16 +4,6 @@ vim.cmd([[set nu]])
 vim.cmd([[set rnu]])
 vim.cmd([[bufdo LspStart]])
 vim.cmd([[command! -nargs=1 G execute ':!git <args>']])
-vim.g.clipboard = {
-	copy = {
-		["+"] = "win32yank.exe -i --crlf",
-		["*"] = "win32yank.exe -i --crlf",
-	},
-	paste = {
-		["+"] = "win32yank.exe -o --lf",
-		["*"] = "win32yank.exe -o --lf",
-	},
-}
 -- PERF: set relative line numbers only in normal mode or command mode
 local set_relativenumber_group = vim.api.nvim_create_augroup("set_relativenumber", { clear = true })
 local set_number_group = vim.api.nvim_create_augroup("set_number", { clear = true })

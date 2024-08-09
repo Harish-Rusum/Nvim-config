d750d2c7920ec2d89af363f6c28b331707a7abd0
harish.rusum@gmail.com <Harish-Rusum>
2024-08-09 17:56:40 +0530

    added kepymaps to save and quit files quickly

--- lua/user/general/keymaps.lua
+++ lua/user/general/keymaps.lua
@@ -70,6 +70,8 @@ vim.keymap.set("c", "W", "w")
 vim.keymap.set("c", "Q", "q")
 vim.keymap.set("n", "s", "^")
 
+vim.keymap.set("n","<leader>w", ":w<CR>")
+vim.keymap.set("n","<leader>q", ":q<CR>")
 
 vim.keymap.set("n", "<leader>cd", ":Trouble<Enter>1<Enter>", { desc = "diagnostics using trouble" })
 
--- lua/user/plugins/auto-session.lua
+++ lua/user/plugins/auto-session.lua
@@ -1,3 +1,2 @@
 -- PERF: setting up auto-session (persisting tabs according to folder)
 
-require("auto-session").setup{}

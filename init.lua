f6e9bda9e35df0443927bbd1e8127402be4d74aa
Harish-Rusum <harish.rusum@gmail.com>
2024-09-21 13:14:24 +0530

    refactored project

--- init.lua
+++ init.lua
@@ -1,49 +1,49 @@
 -- NOTE: loading lazy.nvim stuff
-require("user.lazy.lazy")
+require("lazy.lazy")
 
 -- NOTE: lsp (language-server-protocol) and autocomplete
-require("user.lsp.lsp")
-require("user.lsp.none-ls")
-require("user.lsp.luasnip")
-require("user.lsp.completions")
+require("lsp.lsp")
+require("lsp.none-ls")
+require("lsp.luasnip")
+require("lsp.completions")
 
 -- NOTE: settings and styling
-require("user.general.settings")
-require("user.general.styling")
+require("general.settings")
+require("general.styling")
 
--- NOTE: UI plugins (user-interface)
-require("user.plugins.which-key")
-require("user.plugins.screenkey")
-require("user.plugins.bufferline")
-require("user.plugins.gitsigns")
-require("user.plugins.treesitter")
-require("user.plugins.hicolors")
-require("user.plugins.noice")
+-- NOTE: UI plugins (interface)
+require("plugins.which-key")
+require("plugins.screenkey")
+require("plugins.bufferline")
+require("plugins.gitsigns")
+require("plugins.treesitter")
+require("plugins.hicolors")
+require("plugins.noice")
 
 -- NOTE: search stuff
-require("user.plugins.telescope")
-require("user.plugins.oil")
-require("user.plugins.neo-tree")
-require("user.plugins.numb")
+require("plugins.telescope")
+require("plugins.oil")
+require("plugins.neo-tree")
+require("plugins.numb")
 
--- NOTE: Lualine (change to user.lualine.lualine_sharp if wanted) 
-require("user.lualine.lualine_rounded")
+-- NOTE: Lualine (change to lualine.lualine_sharp if wanted) 
+require("lualine.lualine_rounded")
 
 -- NOTE: git commands
-require("user.git.git")
+require("git.git")
 
 -- NOTE: terminal plugin
-require("user.plugins.fterm")
-require("user.plugins.toggleterm")
+require("plugins.fterm")
+require("plugins.toggleterm")
 
 -- NOTE: AI using avante
-require("user.plugins.avante")
+require("plugins.avante")
 
 -- NOTE: mason for installing dependencies
-require("user.plugins.mason")
+require("plugins.mason")
 
 -- NOTE: plugin for persisting nvim sessions
-require("user.plugins.auto-session")
+require("plugins.auto-session")
 
 -- NOTE:  keymaps for EVERYTHING
-require("user.general.keymaps")
+require("general.keymaps")

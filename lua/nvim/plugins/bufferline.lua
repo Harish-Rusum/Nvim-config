b8e954ffff0751a43bd498ac5e3d55559a35d6a1
Harish-Rusum <harish.rusum@gmail.com>
2024-10-07 12:14:09 +0530

    added some styling

--- lua/nvim/plugins/bufferline.lua
+++ lua/nvim/plugins/bufferline.lua
@@ -1,3 +1,19 @@
 -- PERF: setting up bufferline (list of tabs at the top)
 
-require("bufferline").setup{}
+require("bufferline").setup {
+  options = {
+    indicator_icon = '▎',
+    modified_icon = '●',
+    persist_buffer_sort = true,
+    always_show_bufferline = true,
+    offsets = {
+      {
+        filetype = "neo-tree",
+        text = "File Explorer",
+        highlight = "Directory",
+        text_align = "center",
+        separator = true,
+      }
+    },
+  },
+}

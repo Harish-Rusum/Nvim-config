43c926839d3c2fb3111108d0dfb4889efa60aec8
Harish-Rusum <harish.rusum@gmail.com>
2024-10-16 09:55:27 +0530

    small change

--- lua/nvim/plugins/catppuccin.lua
+++ lua/nvim/plugins/catppuccin.lua
@@ -1,11 +1,13 @@
+-- PERF: catppuccin theme settings
+
 require("catppuccin").setup({
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
+    flavour = "mocha",
+    background = {
         light = "latte",
         dark = "mocha",
     },
-    transparent_background = true, -- disables setting the background color.
-    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+    transparent_background = true,
+    show_end_of_buffer = false,
 })
 
 vim.cmd.colorscheme "catppuccin"

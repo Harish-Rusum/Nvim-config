fd1cb907bde47920aafefd863ddbd0a80ef3ef30
Harish-Rusum <harish.rusum@gmail.com>
2024-10-18 10:16:44 +0530

    Cleaned up settings and styling files

--- lua/nvim/general/settings.lua
+++ lua/nvim/general/settings.lua
@@ -1,5 +1,6 @@
--- PERF: Vim settings
+-- settings.lua
 
+-- PERF: Vim settings
 vim.cmd([[set guicursor=n-v-c:block,i:block]])
 vim.cmd([[set nu]])
 vim.cmd([[set rnu]])
@@ -7,42 +8,63 @@ vim.cmd([[bufdo LspStart]])
 vim.cmd([[vnoremap > >gv]])
 vim.cmd([[vnoremap < <gv]])
 vim.cmd([[set splitright]])
+vim.cmd([[set cursorline]])
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath("config") .. "nvim/undo"
+vim.schedule(function()
+    vim.opt.clipboard = "unnamedplus"
+end)
 
 -- PERF: set relative line numbers only in normal mode or command mode
 local set_relativenumber_group = vim.api.nvim_create_augroup("set_relativenumber", { clear = true })
 local set_number_group = vim.api.nvim_create_augroup("set_number", { clear = true })
 
 local function has_file_path()
-	return vim.api.nvim_buf_get_name(0) ~= ""
+    return vim.api.nvim_buf_get_name(0) ~= ""
 end
 
 vim.api.nvim_create_autocmd({ "VimEnter", "InsertLeave" }, {
-	desc = "set relativenumber",
-	group = set_relativenumber_group,
-	pattern = "*",
-	callback = function()
-		if has_file_path() then
-			vim.opt.relativenumber = true
-		end
-	end,
+    desc = "set relativenumber",
+    group = set_relativenumber_group,
+    pattern = "*",
+    callback = function()
+        if has_file_path() then
+            vim.opt.relativenumber = true
+        end
+    end,
 })
+
 vim.api.nvim_create_autocmd("InsertEnter", {
-	desc = "set number",
-	group = set_number_group,
-	pattern = "*",
-	callback = function()
-		if has_file_path() then
-			vim.opt.relativenumber = false
-			vim.opt.number = true
-		end
-	end,
+    desc = "set number",
+    group = set_number_group,
+    pattern = "*",
+    callback = function()
+        if has_file_path() then
+            vim.opt.relativenumber = false
+            vim.opt.number = true
+        end
+    end,
 })
 
-vim.cmd([[set cursorline]])
-vim.opt.undofile = true
+-- Set indentation for multiple filetypes
+local IndentGroup = vim.api.nvim_create_augroup("CustomIndent", { clear = true })
 
-vim.opt.undodir = vim.fn.stdpath("config") .. "nvim/undo"
-vim.schedule(function()
-	vim.opt.clipboard = "unnamedplus"
-end)
+local function setIndent(filetype, shiftwidth, tabstop)
+    vim.api.nvim_create_autocmd("FileType", {
+        group = IndentGroup,
+        pattern = filetype,
+        callback = function()
+            vim.opt_local.shiftwidth = shiftwidth
+            vim.opt_local.tabstop = tabstop
+        end,
+    })
+end
 
+setIndent("lua", 2, 2)
+setIndent("python", 4, 4)
+setIndent("cpp", 4, 4)
+
+-- Notifications
+require("notify").setup({
+    background_colour = "#000000",
+})
--- lua/nvim/general/styling.lua
+++ lua/nvim/general/styling.lua
@@ -1,106 +1,77 @@
+-- styling.lua
+
 -- PERF: Vim appearance settings
 vim.api.nvim_create_augroup("nobg", { clear = true })
 vim.cmd.colorscheme("catppuccin")
-vim.cmd[[highlight Visual guibg=#2f2f3f guifg=none]]
+vim.cmd([[highlight Visual guibg=#2f2f3f guifg=none]])
 vim.opt.showmode = false
+
 vim.api.nvim_create_autocmd("ColorScheme", {
-	callback = function()
-		vim.api.nvim_set_hl(0, "WhichKeyFloat", { bg = "NONE" })
-	end,
+    callback = function()
+        vim.api.nvim_set_hl(0, "WhichKeyFloat", { bg = "NONE" })
+    end,
 })
-vim.api.nvim_set_hl(0, "BufferLineBackground", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "BufferLineSeparator", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "BufferLineSeparatorVisible", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "BufferLineSeparatorSelected", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "BufferLineCloseButton", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "BufferLineCloseButtonVisible", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "BufferLineCloseButtonSelected", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "BufferLineBufferVisible", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "BufferLineBufferSelected", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "BufferLineFill", { bg = "NONE" })
 
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
-local IndentGroup = vim.api.nvim_create_augroup("CustomIndent", { clear = true })
+-- Highlighting for buffer line components
+local bufferline_highlights = {
+    "BufferLineBackground",
+    "BufferLineSeparator",
+    "BufferLineSeparatorVisible",
+    "BufferLineSeparatorSelected",
+    "BufferLineCloseButton",
+    "BufferLineCloseButtonVisible",
+    "BufferLineCloseButtonSelected",
+    "BufferLineBufferVisible",
+    "BufferLineBufferSelected",
+    "BufferLineFill"
+}
 
-local function setIndent(filetype, shiftwidth, tabstop)
-	vim.api.nvim_create_autocmd("FileType", {
-		group = IndentGroup,
-		pattern = filetype,
-		callback = function()
-			vim.opt_local.shiftwidth = shiftwidth
-			vim.opt_local.tabstop = tabstop
-		end,
-	})
+for _, hl in ipairs(bufferline_highlights) do
+    vim.api.nvim_set_hl(0, hl, { bg = "NONE" })
 end
 
--- Set indentation for multiple filetypes
-setIndent("lua", 2, 2)
-setIndent("python", 4, 4)
-setIndent("cpp", 4, 4)
-require("notify").setup({
-	background_colour = "#000000",
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
-vim.cmd([[highlight WinSeparator guifg=#383646 guibg=none]])
-
--- local function default()
--- 	local telescope_highlights = {
--- 		TelescopePromptTitle = { bg = "none", fg = "#89b5fa" },
--- 		TelescopeResultsTitle = { bg = "none", fg = "#89b5fa" },
--- 		TelescopePreviewTitle = { bg = "none", fg = "#89b5fa" },
--- 		TelescopePromptPrefix = { bg = "none", fg = "#bec6e4", bold = true },
--- 		TelescopePromptNormal = { bg = "none", fg = "#bec6e4", bold = true },
--- 		TelescopeResultsNormal = { bg = "none", fg = "#bec6e4", bold = true },
--- 		TelescopeResultsBorder = { bg = "none", fg = "#89b5fa" },
--- 		TelescopePromptBorder = { bg = "none", fg = "#89b5fa" },
--- 		TelescopePreviewNormal = { bg = "none", fg = "#89b5fa" },
--- 		TelescopePreviewBorder = { bg = "none", fg = "#89b5fa" },
--- 		TelescopeMatching = { bg = "none", fg = "#7a9ee0" },
--- 		TelescopeSelection = { bg = "#26233a", fg = "#bec6e4", bold = true}
--- 	}
---
--- 	for hl, col in pairs(telescope_highlights) do
--- 		vim.api.nvim_set_hl(0, hl, col)
--- 	end
--- end
 
 local function titles()
+    local colors = require("catppuccin.palettes").get_palette("mocha")
 
-	local colors = require("catppuccin.palettes").get_palette("mocha")
+    local function darken(color, percentage)
+        local r = tonumber(color:sub(2, 3), 16)
+        local g = tonumber(color:sub(4, 5), 16)
+        local b = tonumber(color:sub(6, 7), 16)
+        r = math.floor(r * (1 - percentage / 100))
+        g = math.floor(g * (1 - percentage / 100))
+        b = math.floor(b * (1 - percentage / 100))
+        return string.format("#%02x%02x%02x", r, g, b)
+    end
 
-	local function darken(color, percentage)
-		local r = tonumber(color:sub(2, 3), 16)
-		local g = tonumber(color:sub(4, 5), 16)
-		local b = tonumber(color:sub(6, 7), 16)
-		r = math.floor(r * (1 - percentage / 100))
-		g = math.floor(g * (1 - percentage / 100))
-		b = math.floor(b * (1 - percentage / 100))
-		return string.format("#%02x%02x%02x", r, g, b)
-	end
+    local darken_percentage = 1
 
-	local darken_percentage = 1
+    local telescope_highlights = {
+        TelescopePromptTitle = { bg = colors.red, fg = darken(colors.mantle, darken_percentage) },
+        TelescopeResultsTitle = { fg = darken(colors.mantle, darken_percentage), bg = colors.green },
+        TelescopePreviewTitle = { bg = colors.teal, fg = darken(colors.mantle, darken_percentage) },
+        TelescopePromptPrefix = { bg = "none", fg = "#bec6e4", bold = true },
+        TelescopePromptNormal = { bg = "none", fg = "#bec6e4", bold = true },
+        TelescopeResultsNormal = { bg = "none", fg = "#bec6e4", bold = true },
+        TelescopeResultsBorder = { bg = "none", fg = "#89b5fa" },
+        TelescopePromptBorder = { bg = "none", fg = "#89b5fa" },
+        TelescopePreviewNormal = { bg = "none", fg = "#89b5fa" },
+        TelescopePreviewBorder = { bg = "none", fg = "#89b5fa" },
+        TelescopeMatching = { bg = "none", fg = "#7a9ee0" },
+        TelescopeSelection = { bg = "#26233a", fg = "#bec6e4", bold = true }
+    }
 
-	local telescope_highlights = {
-		TelescopePromptTitle = { bg = colors.red, fg = darken(colors.mantle, darken_percentage) },
-		TelescopeResultsTitle = { fg = darken(colors.mantle, darken_percentage), bg = colors.green },
-		TelescopePreviewTitle = { bg = colors.teal, fg = darken(colors.mantle, darken_percentage) },
-		TelescopePromptPrefix = { bg = "none", fg = "#bec6e4", bold = true },
-		TelescopePromptNormal = { bg = "none", fg = "#bec6e4", bold = true },
-		TelescopeResultsNormal = { bg = "none", fg = "#bec6e4", bold = true },
-		TelescopeResultsBorder = { bg = "none", fg = "#89b5fa" },
-		TelescopePromptBorder = { bg = "none", fg = "#89b5fa" },
-		TelescopePreviewNormal = { bg = "none", fg = "#89b5fa" },
-		TelescopePreviewBorder = { bg = "none", fg = "#89b5fa" },
-		TelescopeMatching = { bg = "none", fg = "#7a9ee0" },
-		TelescopeSelection = { bg = "#26233a", fg = "#bec6e4", bold = true}
-	}
-	for hl, col in pairs(telescope_highlights) do
-		vim.api.nvim_set_hl(0, hl, col)
-	end
+    for hl, col in pairs(telescope_highlights) do
+        vim.api.nvim_set_hl(0, hl, col)
+    end
 end
+
 titles()

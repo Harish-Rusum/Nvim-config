eceba34d4b486fd36ae8a2e37a323a6df7f0f2e9
Harish Rusum <harish.rusum@gmail.com>
2025-01-11 22:24:48 +0530

    updated lsp lines

--- lua/nvim/lsp/lines.lua
+++ lua/nvim/lsp/lines.lua
@@ -1,3 +1,10 @@
 -- PERF: In-depth cool lookin' lsp errors
 
 require("lsp_lines").setup()
+
+-- PERF: Remove normal lsp error messages
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = false
+    }
+)
